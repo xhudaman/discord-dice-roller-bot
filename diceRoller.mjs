6f6c1710efc2665b115522fb28291e1d21bd92d9
xhudaman <edell.matthew@gmail.com>
2020-12-15 00:56:14 -0500

    Added dice roller logic and changed jshint to allow regex without u flag

--- diceRoller.mjs
+++ diceRoller.mjs
@@ -1,105 +1,51 @@
-const isDice = string => {
-  // return if string matches dice naming syntax i.e. 1d20
-  return string.match(/\d+[d+-]\d+/);
-};
-
-const getRolledValue = rollArray => {
-  let diceSplit = rollArray[0].split("d");
-  let dCount = Math.min(
-    diceSplit[0].length > 0 ? Number.parseInt(diceSplit[0], 10) : 1,
-    50
-  );
-  let dSize = Number.parseInt(diceSplit[1], 10);
-
-  // modify the result from the roll
-  let mods = [];
-  if (rollArray.length > 1) {
-    for (let i = 1; i < rollArray.length; i++) {
-      mods.push(Number.parseInt(rollArray[i], 10));
-    }
-  }
-  // console.log("getting mods from array of strings...", {
-  // rollArray: rollArray,
-  // mods: mods,
-  // });
-
-  let baseResult;
-  for (let i = 0; i < dCount; ++i) {
-    baseResult = Math.floor(Math.random() * Math.floor(dSize) + 1);
-  }
-  // console.log("got base result of roll...", baseResult);
-
-  // Add all values in mods array together with starting value of our base roll results
-  let modifiedResult = mods.reduce(function(accumulator, currentValue) {
-    return accumulator + currentValue;
-  }, baseResult);
-  // console.log("got base modified of roll...", modifiedResult);
-
-  return {
-    dice: `${diceSplit[0]}d${diceSplit[1]}`,
-    baseResult: baseResult,
-    modifiedResult: modifiedResult,
-    mods: mods
-  };
-};
-
-const getResultString = results => {
-  let total = 0;
-  for (let i = 0; i < results.length; i++) {
-    // console.log("Total before update", total);
-    total += results[i].modifiedResult;
-    // console.log("Total after update", total);
-  }
-
-  let resultString = `${total}`;
-
-  resultString = `${resultString} (${results
-    .map(function(result) {
-      return `([${result.dice.slice(1)}: ${result.baseResult}] ${result.mods
-        .map(function(mod) {
-          return mod < 0 ? `- ${Math.abs(mod)}` : `+ ${Math.abs(mod)}`;
-        })
-        .join(" ")})`;
-    })
-    .join(" + ")})`;
-
-  return resultString;
-};
+const getRandNum = (lowerLimit, upperLimit) =>
+  Math.floor(Math.random() * upperLimit) + lowerLimit;
 
 const diceRoller = input => {
-  const inputSplit = input.split(/\+/).map(function(string) {
-    return string.trim();
-  });
+  // captures syntax 1d20+1 and any variation of it up to 3 digit dice sizes with a min of 1 die
+  const diceAndModifiersRegex = /(\d+\d{1,3})(((\+|\-)\d+)+)?/gi;
 
-  if (!Array.isArray(inputSplit)) {
-    const rollResult = getRolledValue([inputSplit]);
-    return getResultString(rollResult);
+  // captures the dice syntax from the dice and modifier group i.e. 1d20 from 1d20+1
+  const diceSyntaxRegex = /\d{1,3}d\d{1,3}/gi;
+
+  // captures just the modifiers from the full syntax
+  const modifierRegex = /(\+|\-)\d+/g;
+
+  if (!input) {
+    return new TypeError(`Missing input!`);
   }
-  const rolls = inputSplit.reduce(function(accumulator, currentValue) {
-    console.log("reducing input to rolls...", {
-      accumulator: accumulator,
-      currentValue: currentValue
-    });
-    if (isDice(currentValue)) {
-      accumulator.push({ dice: currentValue, mods: [] });
+
+  if (!diceAndModifiersRegex.test(input)) {
+    return new Error("Invalid input format!");
+  }
+
+  // (dice syntax) any digit, letter d, digit between 1-3 characters long, (modifier syntax) + or - a digit one or more times
+  const splitInputToDiceRolls = diceString =>
+    diceString.match(diceAndModifiersRegex);
+
+  const individualRolls = splitInputToDiceRolls(input).reduce(
+    (accumulator, currentValue) => {
+      let dice = currentValue.match(diceSyntaxRegex)[0];
+
+      let modifiers = currentValue.match(modifierRegex);
+      modifiers = modifiers ? modifiers.map(value => parseInt(value)) : [];
+
+      let diceCount = parseInt(dice.match(/\d{1,3}(?=d)/g)[0]);
+      let diceSize = parseInt(dice.match(/(?<=d)\d{1,3}/g)[0]);
+
+      let rolls = { modifiers, [`d${diceSize}`]: [] };
+
+      for (let i = 0; i < diceCount; i++) {
+        rolls[`d${diceSize}`].push(getRandNum(1, diceSize));
+      }
+
+      accumulator.push(rolls);
       return accumulator;
-    }
-    accumulator[accumulator.length - 1].mods.push(currentValue);
-    return accumulator;
-  }, []);
+    },
+    []
+  );
 
-  // console.log("Got dice and mods from input!", {
-  // inputSplit: inputSplit,
-  // rolls: rolls,
-  // });
-
-  const results = rolls.map(roll => {
-    return getRolledValue([roll.dice].concat(roll.mods));
-  });
-
-  // console.log("Got all roll results!", results);
-  //return getResultString(results);
-  return results;
+  return individualRolls;
 };
 
 export default diceRoller;
--- diceRoller.test.js
+++ diceRoller.test.js
@@ -1,14 +1,101 @@
-import rollDice from "./diceRoller";
-
-const validDiceRoll = "1d20";
-const validDiceRollWithModifier = "1d20+1";
-const validDiceRollWithMultipleDice = "2d20";
-const validDiceRollWithMultipleDiceAndModifier = "2d20+1";
+import roll from "./diceRoller";
 
 describe("dice roller", () => {
-  test("falsy value throws error", () => {
+  test("null, false, undefined, and empty string returns error", () => {
     [null, false, undefined, ""].forEach(value => {
-      expect(rollDice(value)).toStrictEqual(new TypeError(`Missing input!`));
+      expect(roll(value)).toStrictEqual(new TypeError(`Missing input!`));
     });
+  });
+
+  test("invalid input format returns error", () => {
+    expect(roll("1dd2+1")).toStrictEqual(new Error("Invalid input format!"));
+  });
+
+  test("input with one die returns correct format", () => {
+    const result = roll("1d20");
+
+    expect(result).toHaveLength(1);
+    expect(result).toStrictEqual([
+      expect.objectContaining({
+        d20: expect.arrayContaining([expect.any(Number)]),
+        modifiers: []
+      })
+    ]);
+    expect(result[0].d20).toHaveLength(1);
+    expect(result[0].modifiers).toHaveLength(0);
+  });
+
+  test("input with one die and modifier returns correct format", () => {
+    const result = roll("1d10+1");
+
+    expect(result).toHaveLength(1);
+    expect(result).toStrictEqual([
+      expect.objectContaining({
+        d10: expect.arrayContaining([expect.any(Number)]),
+        modifiers: expect.arrayContaining([1])
+      })
+    ]);
+    expect(result[0].d10).toHaveLength(1);
+    expect(result[0].modifiers).toHaveLength(1);
+  });
+
+  test("input with multiple dice of same size returns correct format", () => {
+    const result = roll("2d20");
+    expect(result).toHaveLength(1);
+    expect(result).toStrictEqual([
+      expect.objectContaining({
+        d20: expect.arrayContaining([expect.any(Number), expect.any(Number)]),
+        modifiers: []
+      })
+    ]);
+    expect(result[0].d20).toHaveLength(2);
+    expect(result[0].modifiers).toHaveLength(0);
+  });
+
+  test("input with multiple dice of same size and modifier returns correct format", () => {
+    const result = roll("2d20+1");
+
+    expect(result).toHaveLength(1);
+    expect(result).toStrictEqual([
+      expect.objectContaining({
+        d20: expect.arrayContaining([expect.any(Number), expect.any(Number)]),
+        modifiers: expect.arrayContaining([1])
+      })
+    ]);
+    expect(result[0].d20).toHaveLength(2);
+    expect(result[0].modifiers).toHaveLength(1);
+  });
+
+  test("input with negative modifier returns correct format", () => {
+    const result = roll("1d20-1");
+
+    expect(result).toHaveLength(1);
+    expect(result).toStrictEqual([
+      expect.objectContaining({
+        d20: expect.arrayContaining([expect.any(Number)]),
+        modifiers: expect.arrayContaining([-1])
+      })
+    ]);
+    expect(result[0].d20).toHaveLength(1);
+    expect(result[0].modifiers).toHaveLength(1);
+  });
+
+  test("input with two dice sizes returns correct format", () => {
+    const result = roll("1d20 + 1d10");
+
+    expect(result).toHaveLength(2);
+    expect(result).toStrictEqual([
+      expect.objectContaining({
+        d20: expect.arrayContaining([expect.any(Number)]),
+        modifiers: []
+      }),
+      expect.objectContaining({
+        d10: expect.arrayContaining([expect.any(Number)]),
+        modifiers: []
+      })
+    ]);
+    expect(result[0].d20).toHaveLength(1);
+    expect(result[1].d10).toHaveLength(1);
+    expect(result[0].modifiers).toHaveLength(0);
   });
 });
