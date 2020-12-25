f5e7d6aed2fbeb9516355b00e8368bd34e2fd880
xhudaman <edell.matthew@gmail.com>
2020-12-25 01:26:24 -0500

    Added try catch to handle thrown errors

--- index.mjs
+++ index.mjs
@@ -22,60 +22,68 @@ discordClient.on("message", message => {
 
   if (command === "roll" || "r") {
     // Zhu Li, do the thing!
-    const diceRollString = args[0];
-    const resultOfRolls = roll(diceRollString);
-    const total = getTotalFromRolls(resultOfRolls);
+    try {
+      const diceRollString = args[0];
+      const resultOfRolls = roll(diceRollString);
+      const total = getTotalFromRolls(resultOfRolls);
 
-    let favourCheck = rolls => {
-      let averages = [];
+      let favourCheck = rolls => {
+        let averages = [];
 
-      rolls.forEach(currentRoll => {
-        Object.keys(currentRoll).forEach(key => {
-          if (key === "modifiers") {
-            return;
-          }
-          let diceSize = parseInt(key.match(/(?<=d)\d{1,3}/));
-          let totalOfRoll = currentRoll[key].reduce(
-            (accumulator, currentValue) => accumulator + currentValue,
-            0
-          );
+        rolls.forEach(currentRoll => {
+          Object.keys(currentRoll).forEach(key => {
+            if (key === "modifiers") {
+              return;
+            }
+            let diceSize = parseInt(key.match(/(?<=d)\d{1,3}/));
+            let totalOfRoll = currentRoll[key].reduce(
+              (accumulator, currentValue) => accumulator + currentValue,
+              0
+            );
 
-          if (totalOfRoll < (diceSize / 2 + 1) * currentRoll[key].length) {
-            averages.push(false);
-          } else {
-            averages.push(true);
-          }
+            if (totalOfRoll < (diceSize / 2 + 1) * currentRoll[key].length) {
+              averages.push(false);
+            } else {
+              averages.push(true);
+            }
+          });
         });
-      });
 
-      let tally = averages
-        .map(average => (average ? 1 : -1))
-        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        let tally = averages
+          .map(average => (average ? 1 : -1))
+          .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
-      if (tally <= -1) {
-        return "not very favourable";
-      } else if (tally >= 1) {
-        return "quite favourable";
-      } else {
-        return "relatively neutral";
-      }
-    };
+        if (tally <= -1) {
+          return "not very favourable";
+        } else if (tally >= 1) {
+          return "quite favourable";
+        } else {
+          return "relatively neutral";
+        }
+      };
 
-    console.log({ resultOfRolls: JSON.stringify(resultOfRolls), total });
+      console.log({ resultOfRolls: JSON.stringify(resultOfRolls), total });
 
-    let responseEmbed = new MessageEmbed();
-    responseEmbed.setTitle("Tymora's Game of Dice");
-    responseEmbed.setColor("#fd2870");
-    responseEmbed.setDescription(
-      `<@${
-        message.author.id
-      }> tested their luck!  The results are ${favourCheck(
-        resultOfRolls
-      )}!\n Their total was: \`${total}\`` /*+
+      let responseEmbed = new MessageEmbed();
+      responseEmbed.setTitle("Tymora's Game of Dice");
+      responseEmbed.setColor("#fd2870");
+      responseEmbed.setDescription(
+        `<@${
+          message.author.id
+        }> tested their luck!  The results are ${favourCheck(
+          resultOfRolls
+        )}!\n Their total was: \`${total}\`` /*+
         JSON.stringify(resultOfRolls)*/
-    );
+      );
 
-    message.channel.send(responseEmbed);
+      message.channel.send(responseEmbed);
+    } catch (error) {
+      let responseEmbed = new MessageEmbed();
+      responseEmbed.setTitle("Tymora's Game of Dice");
+      responseEmbed.setColor("#fd2870");
+      responseEmbed.setDescription(error.message);
+      message.channel.send(responseEmbed);
+    }
     // message.channel.send(
     //   `@${message.author.username} rolled ${JSON.stringify(resultOfRoll)}`
     // );
